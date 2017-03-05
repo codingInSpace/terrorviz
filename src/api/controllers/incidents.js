5747f316e502f2c4510ea6e3b30c701c536d89dc
codingInSpace <jonathan.grangien@gmail.com>
2017-03-05 19:36:28 +0100

    Remove unneeded api functions

--- src/api/controllers/incidents.js
+++ src/api/controllers/incidents.js
@@ -1,32 +1,6 @@
 import dbContainer from '../db'
 const db = new dbContainer()
 
-// List items
-function list(req, res) {
-  const limit = parseFloat(req.params.limit) || 1000000
-  db.incidents.find().limit(limit).toArray()
-    .then(results => {
-      res.json(results)
-    })
-    .catch(reason => {
-      console.error(reason)
-    })
-}
-
-// Test
-function test(req, res) {
-  db.incidents.find({
-    'qeventid': 201512310037
-  })
-    .toArray()
-    .then(data => {
-      res.json(data)
-    })
-    .catch(reason => {
-      console.error(reason)
-    })
-}
-
 function getYear(req, res) {
 	console.log('get year')
   const year = parseFloat(req.params.y)
@@ -45,8 +19,6 @@ function getYears(req, res) {
 
   if (!years)
     return res.json([{status: 500, msg: 'Undefined input'}])
-
-  console.log(years)
 
   db.incidents.find({
     'iyear': { '$in': years }
@@ -83,8 +55,6 @@ function getYearRange(req, res) {
 }
 
 export default {
-  list,
-  test,
   getYear,
   getYears,
   getYearRange
--- src/api/routes/incidents.js
+++ src/api/routes/incidents.js
@@ -2,14 +2,6 @@ import express from 'express'
 import incidentCtrl from '../controllers/incidents'
 const router = express.Router()
 
-// api/incidents -- list first 10000 items
-router.route('/')
-  .get(incidentCtrl.list)
-
-// api/incidents/xxx -- list all with limit param
-router.route('/:limit')
-  .get(incidentCtrl.list)
-
 // api/incidents/year/xxx -- list all for given year param
 router.route('/year/:y')
   .get(incidentCtrl.getYear)
@@ -22,9 +14,5 @@ router.route('/years')
 // example: api/incidents/yearrange/19982002 for 1998 to and including 2002
 router.route('/yearrange/:range')
   .get(incidentCtrl.getYearRange)
-
-// api/incidents/test -- get one incident to see that it works
-router.route('/test')
-  .get(incidentCtrl.test)
 
 export default router
